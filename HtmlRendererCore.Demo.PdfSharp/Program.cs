b4280a9fd38d813086c172774965c7223288bd54
James Petty <james303petty@hotmail.com>
2021-01-23 15:40:35 +1100

    Setup .net core port

---
+++ HtmlRendererCore.Demo.PdfSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
+
+namespace HtmlRendererCore.Demo.PdfSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var html = @"
+            <html>
+                <body>
+                    <p>Test html document.</p>
+                </body>
+            </html>
+            ";
+
+            Console.WriteLine(html);
+
+            Console.WriteLine("Converting to PDF...");
+
+            var pdf = ConvertHtmlToPdf(html);
+
+            Console.WriteLine(pdf);
+        }
+
+        private static string ConvertHtmlToPdf(string html)
+        {
+            string res = null;
+
+            using (var stream = new MemoryStream())
+            {
+                var pdf = PdfGenerator.GeneratePdf(html, PdfSharpCore.PageSize.A4);
+
+                pdf.Save(stream);
+
+                res = Convert.ToBase64String(stream.ToArray());
+            }
+
+            return res;
+        }
+    }
+}
